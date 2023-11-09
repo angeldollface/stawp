8ff3a47b6119e0a874715cfb15756d1e8979fb3a
angeldollface <officialrealalexanderabraham@gmail.com>
2023-11-10 00:38:20 +0100

    Init.

---
+++ Makefile
@@ -0,0 +1,4 @@
+build: ; gcc main.c -o stawp
+clean: ; rm -rf stawp
+perm: ; chmod +x stawp
+run: ; ./stawp time && ./stawp date
---
+++ src/main.c
@@ -0,0 +1,45 @@
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const char* get_date(){
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char *result = (char*)malloc(10 * sizeof(char));
+    sprintf(result, "%d-%d-%d", tm.tm_mday, tm.tm_mon, tm.tm_year+1900);
+    return result;
+}
+
+const char* get_time(){
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char *result = (char*)malloc(8 * sizeof(char));
+    sprintf(result, "%d:%d:%d", tm.tm_hour, tm.tm_min, tm.tm_sec);
+    return result;
+}
+
+void cli(int argc, char* argv[]){
+    char *err_msg = "Unrecognized arguments!";
+    if (argc == 2){
+        if (strcmp(argv[1], "time") == 0){
+            char *time = get_time();
+            printf("%s\n", time);
+        }
+        else if (strcmp(argv[1], "date") == 0){
+            char *date = get_date();
+            printf("%s\n", date);
+        }
+        else {
+            printf("%s\n", err_msg);
+        }
+    }
+    else {
+        printf("%s\n", err_msg);
+    }
+}
+
+int main(int argc, char* argv[]){
+    cli(argc, argv);
+    return 0;
+}
